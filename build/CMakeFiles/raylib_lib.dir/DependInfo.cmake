0de97ea1bc58859e57b09fa0aa9023c814bc643f
abe-3 <arock@hmc.edu>
2026-06-06 21:42:22 -0400

    Working build raylib ex: core_2d_camera_mouse_zoom

---
+++ build/CMakeFiles/raylib_lib.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/abrahamrock/Documents/Academics/Coding/Raylib-Practice/raylib/src/raudio.c" "CMakeFiles/raylib_lib.dir/raylib/src/raudio.c.o" "gcc" "CMakeFiles/raylib_lib.dir/raylib/src/raudio.c.o.d"
+  "/Users/abrahamrock/Documents/Academics/Coding/Raylib-Practice/raylib/src/rcore.c" "CMakeFiles/raylib_lib.dir/raylib/src/rcore.c.o" "gcc" "CMakeFiles/raylib_lib.dir/raylib/src/rcore.c.o.d"
+  "/Users/abrahamrock/Documents/Academics/Coding/Raylib-Practice/raylib/src/rmodels.c" "CMakeFiles/raylib_lib.dir/raylib/src/rmodels.c.o" "gcc" "CMakeFiles/raylib_lib.dir/raylib/src/rmodels.c.o.d"
+  "/Users/abrahamrock/Documents/Academics/Coding/Raylib-Practice/raylib/src/rshapes.c" "CMakeFiles/raylib_lib.dir/raylib/src/rshapes.c.o" "gcc" "CMakeFiles/raylib_lib.dir/raylib/src/rshapes.c.o.d"
+  "/Users/abrahamrock/Documents/Academics/Coding/Raylib-Practice/raylib/src/rtext.c" "CMakeFiles/raylib_lib.dir/raylib/src/rtext.c.o" "gcc" "CMakeFiles/raylib_lib.dir/raylib/src/rtext.c.o.d"
+  "/Users/abrahamrock/Documents/Academics/Coding/Raylib-Practice/raylib/src/rtextures.c" "CMakeFiles/raylib_lib.dir/raylib/src/rtextures.c.o" "gcc" "CMakeFiles/raylib_lib.dir/raylib/src/rtextures.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/raylib_lib.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raylib_lib.dir/raylib/src/raudio.c.o"
+  "CMakeFiles/raylib_lib.dir/raylib/src/raudio.c.o.d"
+  "CMakeFiles/raylib_lib.dir/raylib/src/rcore.c.o"
+  "CMakeFiles/raylib_lib.dir/raylib/src/rcore.c.o.d"
+  "CMakeFiles/raylib_lib.dir/raylib/src/rmodels.c.o"
+  "CMakeFiles/raylib_lib.dir/raylib/src/rmodels.c.o.d"
+  "CMakeFiles/raylib_lib.dir/raylib/src/rshapes.c.o"
+  "CMakeFiles/raylib_lib.dir/raylib/src/rshapes.c.o.d"
+  "CMakeFiles/raylib_lib.dir/raylib/src/rtext.c.o"
+  "CMakeFiles/raylib_lib.dir/raylib/src/rtext.c.o.d"
+  "CMakeFiles/raylib_lib.dir/raylib/src/rtextures.c.o"
+  "CMakeFiles/raylib_lib.dir/raylib/src/rtextures.c.o.d"
+  "libraylib_lib.a"
+  "libraylib_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/raylib_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
